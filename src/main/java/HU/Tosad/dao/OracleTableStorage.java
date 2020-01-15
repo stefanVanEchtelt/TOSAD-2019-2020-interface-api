ac257a1c1d74f02ff916dc229dbfd2d4d47f56c1
JobStoker <job.stoker@gmail.com>
2020-01-15 14:46:55 +0100

    Niet werkende database

---
+++ src/main/java/HU/Tosad/dao/OracleTableStorage.java
@@ -0,0 +1,38 @@
+package HU.Tosad.dao;
+
+import HU.Tosad.model.Table;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OracleTableStorage implements TableStorage {
+    @Override
+    public List<Table> getAll() {
+        List<Table> tables = new ArrayList<Table>();
+        try (Connection con = OracleConnection.getInstance().getConnection()) {
+            String query =  "SELECT \n" +
+                            "    table_name\n" +
+                            "FROM\n" +
+                            "    dba_tables;";
+            PreparedStatement pstmt = con.prepareStatement(query);
+            ResultSet dbResultSet = pstmt.executeQuery();
+            System.out.println(con);
+            while (dbResultSet.next()) {
+                dbResultSet.getString("table_name");
+                System.out.println(dbResultSet.getString("table_name"));
+            }
+        } catch (SQLException sqle) { sqle.printStackTrace(); }
+
+        return tables;
+    }
+
+    @Override
+    public Table getByName(String name) {
+        return null;
+    }
+}
+
