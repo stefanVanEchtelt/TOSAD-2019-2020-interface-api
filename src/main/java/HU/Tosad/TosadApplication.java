5b46331066c56994587b780077cc3d8c9eae5798
TijsS <tijs.slagt@student.hu.nl>
2020-01-15 10:08:12 +0100

    first backend push

---
+++ src/main/java/HU/Tosad/TosadApplication.java
@@ -0,0 +1,12 @@
+package HU.Tosad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TosadApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TosadApplication.class, args);
+	}
+
+}
